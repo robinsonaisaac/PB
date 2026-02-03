7565f5651b06093659428e19edd02f80b98015b3
Matthew Casey <mattcasey02@gmail.com>
2026-02-02 23:05:06 -0600

    Add all combinations of options to run script

--- PB_scripts/run_all.sh
+++ PB_scripts/run_all.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-sbatch --job-name=ees_approval_none --partition=normal --time=24:00:00 master_script.sh -a ees -u cardinal -c none
+# EES Cardinal (3 completion options, exhaustive/not-exhaustive)
+# sbatch --job-name=ees_approval_none --partition=normal --time=24:00:00 master_script.sh -a ees -u cardinal -c none
+sbatch --job-name=ees_cardinal_add_one --partition=long --time=168:00:00 master_script.sh -a ees -u cardinal -c add-one
+sbatch --job-name=ees_cardinal_add_one_exhaustive --partition=long --time=168:00:00 master_script.sh -a ees -u cardinal -c add-one -e
+sbatch --job-name=ees_cardinal_add_opt --partition=long --time=168:00:00 master_script.sh -a ees -u cardinal -c add-opt
+sbatch --job-name=ees_cardinal_add_opt_exhaustive --partition=long --time=168:00:00 master_script.sh -a ees -u cardinal -c add-opt -e
+sbatch --job-name=ees_cardinal_add_opt_skip --partition=long --time=168:00:00 master_script.sh -a ees -u cardinal -c add-opt-skip
+sbatch --job-name=ees_cardinal_add_opt_skip_exhaustive --partition=long --time=168:00:00 master_script.sh -a ees -u cardinal -c add-opt-skip -e
+
+# EES cost (3 completion options, exhaustive/not-exhaustive)
+sbatch --job-name=ees_cost_none --partition=short --time=1:00:00 master_script.sh -a ees -u cost -c none
+sbatch --job-name=ees_cost_add_one --partition=long --time=168:00:00 master_script.sh -a ees -u cost -c add-one
+sbatch --job-name=ees_cost_add_one_exhaustive --partition=long --time=168:00:00 master_script.sh -a ees -u cost -c add-one -e
+sbatch --job-name=ees_cost_add_opt --partition=long --time=168:00:00 master_script.sh -a ees -u cost -c add-opt
+sbatch --job-name=ees_cost_add_opt_exhaustive --partition=long --time=168:00:00 master_script.sh -a ees -u cost -c add-opt -e
+sbatch --job-name=ees_cost_add_opt_skip --partition=long --time=168:00:00 master_script.sh -a ees -u cost -c add-opt-skip
+sbatch --job-name=ees_cost_add_opt_skip_exhaustive --partition=long --time=168:00:00 master_script.sh -a ees -u cost -c add-opt-skip -e
+
+# MES Cardinal (1 completion option, exhaustive/not-exhaustive)
+sbatch --job-name=mes_cardinal_none --partition=short --time=1:00:00 master_script.sh -a mes -u cardinal -c none
+sbatch --job-name=mes_cardinal_add_one --partition=long --time=168:00:00 master_script.sh -a mes -u cardinal -c add-one
+sbatch --job-name=mes_cardinal_add_one_exhaustive --partition=long --time=168:00:00 master_script.sh -a mes -u cardinal -c add-one -e
+
+# MES cost (1 completion option, exhaustive/not-exhaustive)
+sbatch --job-name=mes_cost_none --partition=short --time=1:00:00 master_script.sh -a mes -u cost -c none
+sbatch --job-name=mes_cost_add_one --partition=long --time=168:00:00 master_script.sh -a mes -u cost -c add-one
+sbatch --job-name=mes_cost_add_one_exhaustive --partition=long --time=168:00:00 master_script.sh -a mes -u cost -c add-one -e
